9d0c46c45847c0ed5ca6c4573d4ac14533175aa8
asimo10 <66620496+asimo10@users.noreply.github.com>
2020-09-12 18:28:47 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
 <?php
 /* START IP LOGGER */
-php include 'config.php';
-if (!empty($_SERVER['HTTP_CLIENT_IP'])) { $ip = $_SERVER['HTTP_CLIENT_IP']; } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) { $ip = $_SERVER['HTTP_X_FORWARDED_FOR']; } else { $ip = $_SERVER['REMOTE_ADDR']; } date_default_timezone_set($timezone); $date = date('Y-m-d H:i:s'); $line_break = "\r\n"; $file = $save_file; $fp = fopen($file, 'a'); fwrite($fp, $line_break); fwrite($fp, $ip . $line_break); fwrite($fp, $date . $line_break); fwrite($fp, "--------------------"); fclose($fp);
+    php include 'config.php';
+      if (!empty($_SERVER['HTTP_CLIENT_IP']))
+    {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    }
+      elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+    {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+      else
+    {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+        date_default_timezone_set($timezone);
+        $date = date('Y-m-d H:i:s');
+        $line_break = "\r\n";
+        $file = $save_file;
+        $fp = fopen($file, 'a');
+        fwrite($fp, $line_break);
+        fwrite($fp, $ip . $line_break);
+        fwrite($fp, $date . $line_break);
+        fwrite($fp, "--------------------");      
+        fclose($fp);
 /* END IP LOGGER */
 ?>
