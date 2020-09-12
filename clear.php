eb1638632bcebdbb33f5e6e58f0404be1a30d0be
asimo10 <66620496+asimo10@users.noreply.github.com>
2020-09-12 20:11:39 +0900

    Update clear.php

--- clear.php
+++ clear.php
@@ -2,8 +2,8 @@
 <link rel="icon" href="https://is5-ssl.mzstatic.com/image/thumb/Purple123/v4/0b/43/c6/0b43c698-fdde-0599-06c0-ac35afd3d496/AppIcon-1x_U007emarketing-0-7-0-85-220.png/246x0w.png">
         <meta http-equiv="refresh" content="0.1;url=../log.php" />
 </head>
+<?php include('config.php'); ?>
 <?php
-include($_SERVER['DOCUMENT_ROOT']."config.php");
 $fp = fopen($save_file, "r+");
 ftruncate($fp, 0);
 fclose($fp);
