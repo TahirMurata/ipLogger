06e394dc7f6048f6f38407d16c4702b531c8e043
asimo10 <66620496+asimo10@users.noreply.github.com>
2020-09-17 19:04:56 +0900

    Create add_this_string.php

---
+++ src/add_this_string.php
@@ -0,0 +1,26 @@
+<?php include('config.php'); ?>
+<?php
+/* START IP LOGGER */
+      if (!empty($_SERVER['HTTP_CLIENT_IP']))
+    {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    }
+      elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+    {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+      else
+    {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+        date_default_timezone_set($timezone);
+        $date = date('Y-m-d H:i:s');
+        $line_break = "\r\n";
+        $fp = fopen($save_file, 'a');
+        fwrite($fp, $line_break);
+        fwrite($fp, $ip . $line_break);
+        fwrite($fp, $date . $line_break);
+        fwrite($fp, "--------------------");      
+        fclose($fp);
+/* END IP LOGGER */
+?>
